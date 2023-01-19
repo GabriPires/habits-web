617a0793b3237ba74cddbbda905f63a3b2654168
Gabriel Pires <gabpiresdev@gmail.com>
2023-01-19 19:38:29 -0300

    fix new habit button on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,8 +10,8 @@ export const Header = () => {
         type="button"
         className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
       >
-        Novo hábito
         <Plus size={20} className="text-violet-500" />
+        Novo hábito
       </button>
     </div>
   );
