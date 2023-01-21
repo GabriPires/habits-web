ed9cb7a6af214651e5d2fc3f8a5455798973d7a9
Gabriel Pires <gabpiresdev@gmail.com>
2023-01-21 11:27:45 -0300

    getting summary from server

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -5,12 +5,14 @@ import clsx from 'clsx';
 import { Check } from 'phosphor-react';
 
 interface HabitProps {
-  completed: number;
-  amount: number;
+  date: Date;
+  completed?: number;
+  amount?: number;
 }
 
-export const HabitDay = ({ amount, completed }: HabitProps) => {
-  const completedPercentage = Math.round((completed / amount) * 100);
+export const HabitDay = ({ amount = 0, completed = 0 }: HabitProps) => {
+  const completedPercentage =
+    amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
   return (
     <Popover.Root>
